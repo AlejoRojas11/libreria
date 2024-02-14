cb96d504561ca84d73a277388efd4ab3e13f63bc
Alejandro <tortugal2004@gmail.com>
2024-02-14 15:30:25 -0500

    control

--- server/index.js
+++ server/index.js
@@ -1,7 +1,42 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
+const fs = require('fs')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(cors())
+
+let userFilePath = path.join(__dirname, 'usuariosRegistrados.json')
+let userJSON= fs.readFileSync(userFilePath, 'utf8')
+let users = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'))
+console.log(users.length)
+
 app.get("/", (req, res)=>{
     res.send("Saludando desde el puerto")
+})
+
+app.post('/registro-usuario', function(req, res){
+    console.log(req.body)
+    let ultimo = users.length
+    let usuarioNuevo={
+        id: ultimo + 1,
+        nombres: req.body.nombres,
+        apellidos: req.body.apellidos,
+        email: req.body.email,
+        password: req.body.password,
+        fecha_creacion:new Date()
+    }
+    let userNuevo
+    if(userJSON == ""){let userNuevo=[]}
+    else{  userNuevo = JSON.parse(userJSON)}
+    userNuevo.push(usuarioNuevo)
+    let userAux = JSON.stringify(userNuevo, null, "\t")
+    fs.writeFileSync(userFilePath, userAux)
 })
 
 const PORT = 3001
--- src/componentes/Registro.js
+++ src/componentes/Registro.js
@@ -2,17 +2,19 @@ import React from 'react'
 import Footer from './footer/Footer'
 import Header from './header/Header'
 
+import react, { useState } from 'react'
+
 function Registro() {
 
     // Se declara una variable Values
     const [values, setValues]= useState({
-        nombre:"",
-        apellido:"",
+        nombres:"",
+        apellidos :"",
         email: "",
         password: ""
     })
 
-    const handleChange = (e) => {
+    const handleChange = (e) => { //Cuando se cambie el input entonces se guarda la información de las variables
         const {name, value} =e.target
         const newValues ={
             //Spread operator
@@ -24,11 +26,9 @@ function Registro() {
     
         const handleSubmit = (e) => {
             e.preventDefault()
-            fetch('http://localhost:3001/registro-usurio',{
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json', 'Accept': 'application/json'
-                },
+            fetch('http://localhost:3001/registro-usuario',{
+                method: 'POST',
+                headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
                 body: JSON.stringify(values)
             })
 
@@ -55,46 +55,21 @@ function Registro() {
                             <form onSubmit={handleSubmit}>
                                 
                                 <div class="form-row">
+                                <div class="form-group col-md-6">
+                                        <label for="inputEmail4">Nombre</label>
+                                        <input type="text" className="form-control" id="input1" placeholder="Nombre" name='nombres' />
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="inputEmail4">Apellidos</label>
+                                        <input type="text" className="form-control" id="input2" placeholder="Email" name='apellidos' />
+                                    </div>
                                     <div class="form-group col-md-6">
                                         <label for="inputEmail4">Email</label>
-                                        <input type="email" class="form-control" id="inputEmail4" placeholder="Email" />
+                                        <input type="email" className="form-control" id="input3" placeholder="Email" name='email'/>
                                     </div>
                                     <div class="form-group col-md-6">
                                         <label for="inputPassword4">Password</label>
-                                        <input type="password" class="form-control" id="inputPassword4" placeholder="Password" />
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="inputAddress">Address</label>
-                                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" />
-                                </div>
-                                <div class="form-group">
-                                    <label for="inputAddress2">Address 2</label>
-                                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
-                                </div>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="inputCity">City</label>
-                                        <input type="text" class="form-control" id="inputCity" />
-                                    </div>
-                                    <div class="form-group col-md-4">
-                                        <label for="inputState">State</label>
-                                        <select id="inputState" class="form-control">
-                                            <option selected>Choose...</option>
-                                            <option>...</option>
-                                        </select>
-                                    </div>
-                                    <div class="form-group col-md-2">
-                                        <label for="inputZip">Zip</label>
-                                        <input type="text" class="form-control" id="inputZip" />
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" id="gridCheck" />
-                                        <label class="form-check-label" for="gridCheck">
-                                            Check me out
-                                        </label>
+                                        <input type="password" className="form-control" id="input4" placeholder="Password" name='password' />
                                     </div>
                                 </div>
                                 <button type="submit" class="btn btn-primary">Sign in</button>
