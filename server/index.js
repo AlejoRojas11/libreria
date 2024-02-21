96c66cda29a1d22c4455af0f28e615f2a533a483
Alejandro <tortugal2004@gmail.com>
2024-02-21 13:29:34 -0500

    funcionado el registro

--- server/index.js
+++ server/index.js
@@ -1,45 +1,50 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const cors = require('cors')
 const fs = require('fs')
 const path = require('path')
 const bodyParser = require('body-parser')
 
-
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(cors())
 
 let userFilePath = path.join(__dirname, 'usuariosRegistrados.json')
-let userJSON= fs.readFileSync(userFilePath, 'utf8')
+let userJSON = fs.readFileSync(userFilePath, 'utf-8')
 let users = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'))
 console.log(users.length)
 
-app.get("/", (req, res)=>{
-    res.send("Saludando desde el puerto")
+
+app.get("/", (req, res) => {
+    res.send(userJSON)
 })
 
-app.post('/registro-usuario', function(req, res){
-    console.log(req.body)
+app.post('/registro-usuario', function (req, res) {
+    
     let ultimo = users.length
-    let usuarioNuevo={
+    let usuarioNuevo = {
         id: ultimo + 1,
         nombres: req.body.nombres,
         apellidos: req.body.apellidos,
         email: req.body.email,
         password: req.body.password,
-        fecha_creacion:new Date()
+        fecha_creación: new Date()
     }
+
     let userNuevo
-    if(userJSON == ""){let userNuevo=[]}
-    else{  userNuevo = JSON.parse(userJSON)}
+    if (userJSON === "") { let userNuevo = [] }
+    else { userNuevo = JSON.parse(userJSON) }
+
     userNuevo.push(usuarioNuevo)
     let userAux = JSON.stringify(userNuevo, null, "\t")
     fs.writeFileSync(userFilePath, userAux)
+
+    res.redirect('/')
+
 })
 
 const PORT = 3001
-app.listen(PORT, ()=>{
-    console.log("Servidor corriendo en el puerto", PORT )
+app.listen(PORT, () => {
+    console.log("Servidor corriendo en el puerto ", PORT)
 })
--- src/componentes/Registro.js
+++ src/componentes/Registro.js
@@ -1,83 +1,76 @@
-import React from 'react'
-import Footer from './footer/Footer'
-import Header from './header/Header'
-
-import react, { useState } from 'react'
+import React, { useState } from 'react';
+import Footer from './footer/Footer';
+import Header from './header/Header';
 
 function Registro() {
-
-    // Se declara una variable Values
-    const [values, setValues]= useState({
-        nombres:"",
-        apellidos :"",
+    const [values, setValues] = useState({
+        nombres: "",
+        apellidos: "",
         email: "",
         password: ""
-    })
 
-    const handleChange = (e) => { //Cuando se cambie el input entonces se guarda la información de las variables
-        const {name, value} =e.target
-        const newValues ={
-            //Spread operator
+    })
+    const handleChange = (e) => { //cuando se cambie de Input entonces se guarda la información en la variables.
+
+        const { name, value } = e.target
+        const newValues = {
             ...values,
             [name]: value,
         }
         setValues(newValues)
-        }
-    
-        const handleSubmit = (e) => {
-            e.preventDefault()
-            fetch('http://localhost:3001/registro-usuario',{
-                method: 'POST',
-                headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
-                body: JSON.stringify(values)
-            })
-
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        fetch('http://localhost:3001/registro-usuario', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json", 'Accept': 'application/json' },           
+            body: JSON.stringify(values)
+        })
             .then(response => {
                 if (response.status === 200) {
-                    alert('Usuario creado con exito')
+                    alert("Usuario creado con éxito")
                 }
                 else {
-                    alert('No fue posible crear el usuario '+ response.status)
-                }
+                    alert("No fue posible crear el usuario " + response.status)
 
+                }
             })
-            .catch((error)=>{
-                alert("No fue posible finalizar el proceso de registro por un error "+ error)
+            .catch((error) => {
+                alert("No fue posible finalizar el proceso de registro por un error " + error)
             })
-        }
+
+
+    }
 
     return (
         <div>
             <Header />
-            <div class="container my-5 py-5">
-
-                            <br></br>
-                            <form onSubmit={handleSubmit}>
-                                
-                                <div class="form-row">
-                                <div class="form-group col-md-6">
-                                        <label for="inputEmail4">Nombre</label>
-                                        <input type="text" className="form-control" id="input1" placeholder="Nombre" name='nombres' />
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="inputEmail4">Apellidos</label>
-                                        <input type="text" className="form-control" id="input2" placeholder="Email" name='apellidos' />
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="inputEmail4">Email</label>
-                                        <input type="email" className="form-control" id="input3" placeholder="Email" name='email'/>
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="inputPassword4">Password</label>
-                                        <input type="password" className="form-control" id="input4" placeholder="Password" name='password' />
-                                    </div>
-                                </div>
-                                <button type="submit" class="btn btn-primary">Sign in</button>
-                            </form>
+            <div className="container my-5 py-5">
+                <form onSubmit={handleSubmit}>
+                    <div className="form-row">
+                        <div className="form-group col-md-6">
+                            <label htmlFor="inputNombre">Nombre</label>
+                            <input type="text" className="form-control" id="inputNombre" placeholder="Nombre" name="nombres" onChange={handleChange} />
                         </div>
+                        <div className="form-group col-md-6">
+                            <label htmlFor="inputApellidos">Apellidos</label>
+                            <input type="text" className="form-control" id="inputApellidos" placeholder="Apellidos" name="apellidos" onChange={handleChange} />
+                        </div>
+                        <div className="form-group col-md-6">
+                            <label htmlFor="inputEmail">Email</label>
+                            <input type="email" className="form-control" id="inputEmail" placeholder="Email" name="email" onChange={handleChange} />
+                        </div>
+                        <div className="form-group col-md-6">
+                            <label htmlFor="inputPassword">Password</label>
+                            <input type="password" className="form-control" id="inputPassword" placeholder="Password" name="password" onChange={handleChange} />
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Registrarse</button>
+                </form>
+            </div>
             <Footer />
         </div>
-    )
+    );
 }
 
-export default Registro
+export default Registro;
